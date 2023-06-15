9c41440ffae0d59ff1a3a19ffdb3c36e3e6398a6
moviefan322 <philman202@yahoo.com>
2023-06-15 10:54:51 -0400

    adds stats module, services, controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { LessonsModule } from './lessons/lessons.module';
+import { StatsModule } from './stats/stats.module';
 
 @Module({
-  imports: [UsersModule, LessonsModule],
+  imports: [UsersModule, LessonsModule, StatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
