cd499a6faa2d609e5cee35edc45fc0f7922d263a
moviefan322 <philman202@yahoo.com>
2023-06-15 10:46:48 -0400

    adds controller, module, services for user and lessons

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { LessonsModule } from './lessons/lessons.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, LessonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
