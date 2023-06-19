962d70c2149b420a1edfd23b088e86ec4953ea85
moviefan322 <philman202@yahoo.com>
2023-06-18 22:26:43 -0400

    lesson GET paths functioning

--- server/src/lessons/lessons.controller.ts
+++ server/src/lessons/lessons.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { LessonsService } from './lessons.service';
 
 @Controller('lessons')
-export class LessonsController {}
+export class LessonsController {
+  constructor(private lessonsService: LessonsService) {}
+  @Get()
+  getLessons() {
+    return this.lessonsService.getAllLLessons();
+  }
+
+  @Get('/:lessonId')
+  getLesson(@Param('lessonId') lessonId: string) {
+    return this.lessonsService.getLesson(lessonId);
+  }
+}
--- server/src/lessons/lessons.service.ts
+++ server/src/lessons/lessons.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { readFile } from 'fs';
+import { join } from 'path';
+
+const filePath = join(__dirname, '..', '..', 'lessonData', 'unit1.json');
+
+const getLessons = async () => {
+  const unit1 = await new Promise((resolve, reject) => {
+    readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+        return;
+      }
+      console.log(data);
+      resolve(data);
+    });
+  });
+
+  const allLessons = [unit1];
+  return allLessons;
+};
 
 @Injectable()
-export class LessonsService {}
+export class LessonsService {
+  async getAllLLessons() {
+    const allLessons = await getLessons();
+    return JSON.stringify(allLessons);
+  }
+
+  async getLesson(lessonId: string) {
+    const allLessons = await getLessons();
+    return JSON.stringify(allLessons[+lessonId - 1]);
+  }
+}
