49a1c825c086a4e67028fcca469bee9fcf1eff60
moviefan322 <philman202@yahoo.com>
2023-06-22 14:12:39 -0400

    adds robust user context with typesafety

--- client/context/UserContext.tsx
+++ client/context/UserContext.tsx
@@ -1,4 +1,35 @@
-import { createContext } from "react";
+import { createContext, useState, useContext } from "react";
 
-export const UserContext = createContext(null);
-export const UserDispatchContext = createContext(null);
+export const CurrentUserContext = createContext({});
+
+interface CurrentUserProviderProps {
+  children: React.ReactNode;
+}
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+}
+
+interface APIResponse {
+  user: User;
+}
+
+export const CurrentUserProvider = ({ children }: CurrentUserProviderProps) => {
+  const [currentUser, setCurrentUser] = useState<User | null>(null);
+
+  const fetchCurrentUser = async () => {
+    let response = await fetch("/api/auth/getme");
+    const data: APIResponse = await response.json();
+    setCurrentUser(data.user);
+  };
+
+  return (
+    <CurrentUserContext.Provider value={{ currentUser, fetchCurrentUser }}>
+      {children}
+    </CurrentUserContext.Provider>
+  );
+};
+
+export const useCurrentUser = () => useContext(CurrentUserContext);
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,16 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import Layout from "@/components/layout/layout";
+import { UserContext } from "@/context/UserContext";
 
 export default function App({ Component, pageProps }: AppProps) {
+  
   return (
-    <Layout>
-      <main className="container">
-        <Component {...pageProps} />
-      </main>
-    </Layout>
+      <Layout>
+        <main className="container">
+          <Component {...pageProps} />
+        </main>
+      </Layout>
   );
 }
