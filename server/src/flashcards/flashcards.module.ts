9acc37c0cefc1c970981ba83e4af121bdef0b960
moviefan322 <philman202@yahoo.com>
2023-06-26 16:01:51 -0400

    configures flashcard.module with typeOrm

--- server/src/flashcards/flashcards.module.ts
+++ server/src/flashcards/flashcards.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FlashcardsController } from './flashcards.controller';
 import { FlashcardsService } from './flashcards.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Flashcard } from './flashcard.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Flashcard])],
   controllers: [FlashcardsController],
-  providers: [FlashcardsService]
+  providers: [FlashcardsService],
 })
 export class FlashcardsModule {}
