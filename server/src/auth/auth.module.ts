2343b44fdc225cfc85af5a4a3eca4196c27660a6
moviefan322 <philman202@yahoo.com>
2023-06-27 11:03:18 -0400

    configures jwtmodule in authmodule

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -3,9 +3,19 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { LocalStrategy } from './local.strategy';
 import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
