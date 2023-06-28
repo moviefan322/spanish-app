4da81e8f43011abeb16e959e09e9ea2a16ac566c
moviefan322 <philman202@yahoo.com>
2023-06-28 11:38:09 -0400

    adds boilerplate for flashcard page

---
+++ client/components/flashcards/flashcard-main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FlashcardMain() {
+  return (
+    <div>
+      <h1>FLASHCARD PAGE</h1>
+    </div>
+  )
+}
+
+export default FlashcardMain
---
+++ client/pages/flashcards.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Flashcards() {
+  return <></>;
+}
+
+export default Flashcards;
