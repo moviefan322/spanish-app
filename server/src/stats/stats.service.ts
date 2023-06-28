fa62a005f73fd43d1a04074feec4da00d64deabf
moviefan322 <philman202@yahoo.com>
2023-06-28 19:58:06 -0400

    creates services for stats

--- server/src/stats/stats.service.ts
+++ server/src/stats/stats.service.ts
@@ -1,6 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Stats } from './stats.entity';
+import { CreateStatDto } from './dtos/create-stat.dto';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+  constructor(
+    @InjectRepository(Stats)
+    private repo: Repository<Stats>,
+  ) {}
+
+  async create(statDto: CreateStatDto) {
+    const stat = this.repo.create(statDto);
+    return this.repo.save(stat);
+  }
+
+  async findStats(userId: number) {
+    return this.repo.find({ where: { userId } });
+  }
+}
