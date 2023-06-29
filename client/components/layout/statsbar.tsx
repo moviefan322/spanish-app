680647d0514979a52afb8fb4e23702bd69293bb5
moviefan322 <philman202@yahoo.com>
2023-06-29 19:31:18 -0400

    updates user state

--- client/components/layout/statsbar.tsx
+++ client/components/layout/statsbar.tsx
@@ -1,10 +1,66 @@
-import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import Link from "next/link";
 import styles from "./statsbar.module.css";
 
+interface Stats {
+  stats: Stat[];
+}
+
+interface Stat {
+  lessonId: number;
+  score: number;
+  outOf: number;
+  userId: number;
+}
+
 function Statsbar() {
   const state = useSelector((state: any) => state.user);
+
+  const stats: Stat[] = state.stats;
+
+  const renderCurrentLesson = (stats: Stat[]) => {
+    const highestLessonIdObject = stats.reduce(
+      (maxObj: Stat, currentObj: Stat) => {
+        if (currentObj.lessonId > maxObj.lessonId) {
+          return currentObj;
+        }
+        return maxObj;
+      }
+    );
+    return highestLessonIdObject;
+  };
+
+  const formatCurrentLesson = (stats: Stat[]) => {
+    const stringId = renderCurrentLesson(stats).lessonId.toString();
+    if (stringId[0] === "0") {
+      return stringId[1];
+    } else {
+      return stringId[0];
+    }
+  };
+
+  const renderPoints = (stats: Stat[]) => {
+    const pointsEarned = stats.reduce((total: number, currentObj: Stat) => {
+      return total + currentObj.score;
+    }, 0);
+    return pointsEarned;
+  };
+
+  const renderOutOfs = (stats: Stat[]) => {
+    const pointsPossible = stats.reduce((total: number, currentObj: Stat) => {
+      return total + currentObj.outOf;
+    }, 0);
+    return pointsPossible;
+  };
+
+  const getPct = () => {
+    const pointsEarned = renderPoints(stats);
+    const pointsPossible = renderOutOfs(stats);
+    const pct = ((pointsEarned / pointsPossible) * 100).toFixed(1);
+    return pct;
+  };
+
+  console.log(stats);
 
   return (
     <>
@@ -13,9 +69,11 @@ function Statsbar() {
           <div>
             <strong>{state.user.username}</strong>
           </div>
-          <div>Unit: 0/20</div>
-          <div>Points: 0/525</div>
-          <div>0%</div>
+          <div>Unit: {formatCurrentLesson(stats)}/20</div>
+          <div>
+            Points: {renderPoints(stats)}/{renderOutOfs(stats)}
+          </div>
+          <div>{getPct()}%</div>
           <Link className={styles.link} href="/flashcards">
             Flashcards
           </Link>
