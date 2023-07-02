3524988d1a4267e9167f430d6f538cfa8a4e56ea
moviefan322 <philman202@yahoo.com>
2023-07-02 06:48:36 -0400

    adds interfaces to authSlice and extraReducers

--- client/features/auth/authSilce.ts
+++ client/features/auth/authSilce.ts
@@ -1,18 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
+import { registerUser } from "@/features/auth/authActions";
+import User from "@/types/User";
+import Flashcard from "@/types/Flashcard";
+import Stats from "@/types/Stats";
 
-const initialState = {
-  loading: false,
-  userInfo: {}, // for user object
-  userToken: null, // for storing the JWT
-  error: null,
-  success: false, // for monitoring the registration process.
+interface AuthState {
+  loading: boolean;
+  user: User | null;
+  flashcards: Flashcard[] | null;
+  stats: Stats[] | null;
+  userToken: string | null;
+  error: string | null | unknown;
+  success: boolean;
 }
 
+const initialState: AuthState = {
+  loading: false,
+  user: null,
+  flashcards: null,
+  stats: null,
+  userToken: null,
+  error: null,
+  success: false,
+};
+
 const authSlice = createSlice({
-  name: 'auth',
+  name: "auth",
   initialState,
   reducers: {},
-  extraReducers: {},
-})
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state, action) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+      })
+      .addCase(registerUser.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      });
+  },
+});
 
-export default authSlice.reducer
+export default authSlice.reducer;
