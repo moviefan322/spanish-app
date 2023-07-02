1b816891702b3179b75a49bfe3589c334d3543f9
moviefan322 <philman202@yahoo.com>
2023-07-02 10:07:17 -0400

    polls for userData from navbar with authApi middleware

--- client/components/layout/navbar.tsx
+++ client/components/layout/navbar.tsx
@@ -1,12 +1,20 @@
 import styles from "./navbar.module.css";
 import Link from "next/link";
+import { useGetUserDetailsQuery } from "@/services/auth/authService";
 import { useSelector, useDispatch } from "react-redux";
 import { setState } from "../../store/userSlice";
 
 function Navbar(): JSX.Element {
   const state = useSelector((state: any) => state.auth);
-
   const dispatch = useDispatch<any>();
+
+  // automatically authenticate user if token is found
+  const { data, isFetching } = useGetUserDetailsQuery("userDetails", {
+    // perform a refetch every 15mins
+    pollingInterval: 10000,
+  });
+
+  console.log(data); // user object
 
   const logoutButtonHandler = (e: React.MouseEvent<HTMLAnchorElement>) => {
     e.preventDefault();
