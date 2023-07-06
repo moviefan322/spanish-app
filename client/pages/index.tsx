ee4e120b5b8d03494f1914b68ce9228921b147ca
moviefan322 <philman202@yahoo.com>
2023-07-05 22:08:46 -0400

    adds .jsx extension to imports

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,7 @@
-import Welcome from "../components/home/welcome";
+import Welcome from "../components/home/welcome.jsx";
 import Head from "next/head";
-import FreeAndFun from "../components/home/free-and-fun";
-import UserTestimonials from "../components/home/user-testimonials";
+import FreeAndFun from "../components/home/free-and-fun.jsx";
+import UserTestimonials from "../components/home/user-testimonials.jsx";
 
 export default function Home() {
   return (
