e1aa8cd522d8364c7387edf0e2dbe4f3c87a5560
moviefan322 <philman202@yahoo.com>
2023-07-05 21:44:34 -0400

    changes ../ to @/

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,7 @@
-// @ts-ignore
-import Welcome from "../components/home/welcome";
+import Welcome from "@/components/home/welcome";
 import Head from "next/head";
-// @ts-ignore
-import FreeAndFun from "../components/home/free-and-fun";
-// @ts-ignore
-import UserTestimonials from "../components/home/user-testimonials";
+import FreeAndFun from "@/components/home/free-and-fun";
+import UserTestimonials from "@/components/home/user-testimonials";
 
 export default function Home() {
   return (
